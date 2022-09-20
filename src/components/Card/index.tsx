782b2074367d556626bdecd7a48ca9214af03587
Arnaldo Neto <costanetoow@gmail.com>
2022-09-20 10:22:20 -0300

    fix: fixes preventDefault bug

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import style from './Card.module.scss';
+import React from 'react'
+
+import style from './Card.module.scss'
 
 const Card: React.FC = ({ children }) => {
-  return (<div className={style.Card}>
-    {children}
-  </div>)
+  return <div className={style.Card}>{children}</div>
 }
 
 export default Card
