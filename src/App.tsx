0eb8f4d28eaa60a6d818159dfcc4fdaabcff4c10
Arnaldo Neto <costanetoow@gmail.com>
2022-09-20 16:10:33 -0300

    feat: show state changes with debug observer

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { RecoilRoot } from 'recoil'
 import style from './App.module.scss'
 import Calendario from './components/Calendario'
 import Card from './components/Card'
+import DebugObserver from './components/DebugObserver'
 import Formulario from './components/Formulario'
 import ListaDeEventos from './components/ListaDeEventos'
 
 function App() {
   return (
     <RecoilRoot>
+      <DebugObserver />
       <Suspense fallback="Está carregando">
         <div className={style.App}>
           <div className={style.Coluna}>
