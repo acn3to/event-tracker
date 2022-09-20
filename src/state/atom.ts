267135e4c297f66a5fb76f881c4f590bb8d75436
Arnaldo Neto <costanetoow@gmail.com>
2022-09-20 14:42:53 -0300

    feat: creates a events filter atom

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,6 +1,7 @@
 import { atom } from 'recoil'
 
 import { IEvento } from '../interfaces/IEvento'
+import { IFiltroDeEventos } from '../interfaces/IFiltroDeEventos'
 
 export const listaDeEventosState = atom<IEvento[]>({
   key: 'listaDeEventosState',
@@ -20,4 +21,9 @@ export const listaDeEventosState = atom<IEvento[]>({
       id: 1642342959,
     },
   ],
+})
+
+export const filtroDeEventos = atom<IFiltroDeEventos>({
+  key: 'filtroDeEventos',
+  default: {},
 })
